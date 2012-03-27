043e2d134e7b25fd3df24d7e47a6e6e18146c30c
diana.kposavec <diana.kposavec@ea8f855e-3231-f43c-0cba-ebcabba4a3a6>
2012-03-27 14:25:35 +0000

    za sada rade gumbi, sad idem pogledati kako povezati akciju klika na gumb sa pozivanjem nekog drugog viewa

--- src/hr/fer/asc/TalkTVstart.java
+++ src/hr/fer/asc/TalkTVstart.java
@@ -2,12 +2,40 @@ package hr.fer.asc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TalkTVstart extends Activity {
     /** Called when the activity is first created. */
+	
+	Button btnUsername; 
+	Button btnKanali;
+	TextView txtGreet;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        btnUsername = (Button) findViewById(R.id.btnUser);
+        btnKanali = (Button) findViewById(R.id.btnKanali);
+        txtGreet = (TextView) findViewById(R.id.txtGreet);
+        
+        btnUsername.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				txtGreet.setText("dogodio se neparni klik");				
+			}
+		});
+        btnKanali.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				txtGreet.setText("dogodio se parni klik");				
+			}
+		});
     }
 }
